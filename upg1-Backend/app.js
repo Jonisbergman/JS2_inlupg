f34a11fcd3093e67c33d6a8ac2c57b19cb01a0bc
Jonis Bergman <jonis.bergman@utb.ecutbildning.se>
2022-08-27 21:21:56 +0200

    klar m backend

--- upg1-Backend/app.js
+++ upg1-Backend/app.js
@@ -3,19 +3,14 @@ const app = express();
 const productController = require('./controllers/productController')
 const cors  = require('cors');
 
-// 
-
 // MIDDLEWARE
+
 app.use(cors());
 app.use(express.urlencoded({  extended: false }));
 app.use(express.json());
 
-
 // CONTROLLERS
-
 app.use('/api/products', productController);
-
-
 
 
 module.exports = app;
